56bdd20e6b9832d117e0dfa15bdc2d58471d518f
Jorge <jhevia@outlook.com>
2017-09-22 11:57:04 +0200

    initial python code updated

---
+++ p1/pythonCode/adaline.py
@@ -0,0 +1,34 @@
+# Author: Jorge Hevia Moreno
+# Contac: jmhev@outlook.com
+# File: this file provides the functions needed to import
+#		data from a csv file and use it
+
+# library so we can read a csv 
+# Installation: sudo pip install pandas
+import pandas as pd
+# module to read arguments
+import sys
+
+if len(sys.argv) < 2:
+	print("Error: the program has less than 1 argument.")
+	print("Usage: python adaline.py <file>")
+	sys.exit()
+
+print("File: " + str(sys.argv[1]))
+# Read the CSV into a panda's data frame (df)
+dataFrame = pd.read_csv(str(sys.argv[1]), delimiter=',')
+
+# Export it as a list of tuples
+data = [tuple(x) for x in dataFrame.values]
+
+
+# ADALINE MODUS OPERANDI
+'''
+ 1) Initialize random weights and threshold
+ 2) Show an initial input patron
+ 3) Calculate the output, compare it with the desired output and get the difference
+ 4) Re-calculate all the weights and threshold
+ 5) Modify the weights and threshold
+ 6) Repeat 2-5 for 1 cycle
+ 7) Repeat 2-6 until input criteria is met
+'''
