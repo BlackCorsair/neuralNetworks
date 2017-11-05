9d15aafc49639e80353ec91053125f82d8413a91
jorge hevia <jmhev@outlook.com>
2017-11-05 12:09:56 +0100

    created memoria.docx

--- p1/pythonCode/adaline.py
+++ p1/pythonCode/adaline.py
@@ -47,13 +47,7 @@ for x in xrange(2,4):
 		print(colors.FAIL  + "Error: that's not a csv file.")
 		print(colors.WARNING  + "File entered: "+ str(sys.argv[x])  + colors.ENDC)
 		sys.exit(2)
-''' old error reporting
-if ".csv" not in str(sys.argv[1]) and :
-	print(colors.FAIL  + "Error: that's not a csv file.")
-	print(colors.WARNING  + "File entered: "+ str(sys.argv[1])  + colors.ENDC)
-	sys.exit(2)
-end of old error reporting
-'''
+
 print(colors.OKBLUE+"Reading files: " + str(sys.argv[1]) + str(sys.argv[2]) + str(sys.argv[3]) + "..." +colors.ENDC)
 # Read the CSV into a panda's data frame (df)
 dataFrameTraining = pd.read_csv(str(sys.argv[1]), delimiter=',')
@@ -209,16 +203,7 @@ weights, threshold = initializeWT(weights, threshold)
 rows = getRows(dataFrameTraining)
 rows_validation = getRows(dataFrameValidation)
 rows_tests = np.array(dataFrameTests)
-# calculates error
-#accumulatedError = calculateError(rows, weights, threshold)
-#print(accumulatedError)
-'''
-print("\n")
-weights, threshold = training(rows, weights, threshold, learnfactor)
-print(colors.OKGREEN+"first weights calc: " + colors.OKBLUE+ str(weights) + ", threshold: " + str(threshold)+ colors.ENDC)
-weights, threshold = training(rows, weights, threshold, learnfactor)
-print(colors.OKGREEN+"weights after training: "+ colors.OKBLUE+ str(weights) + ", threshold: " + str(threshold)+ colors.ENDC)
-'''
+
 print(colors.OKBLUE+"Training..." +colors.ENDC)
 
 errorTraining = []
@@ -235,13 +220,12 @@ print(colors.OKBLUE+"Predicted output saved." +colors.ENDC)
 baseOutput = []
 baseOutputVal = []
 
-#plt.xlim([0,1000])
 plt.xlabel('Iterations from 0 to '+ sys.argv[5])
 plt.ylabel('Error across the Iterations')
-#plt.plot(errorTraining, 'b', c='b', label="Training error", errorValidated, 'r', c='r', label="Validation error")
 plt.plot(errorTraining, c='b', label="Training error")
 plt.plot(errorValidated, c='r', label="Validation error")
 plt.legend()
+
 # WRITING FILES
 outputsTxt = 'outputs-nIterations-'+sys.argv[5]+'-learnfactor-'+str(learnfactor)+'.csv'
 weightsTxt = 'weights-nIterations-'+sys.argv[5]+'-learnfactor-'+str(learnfactor)+'.csv'
